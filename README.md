99b5836c38c378c975efa714fb243b51dc8d88cb
michal <michal.bella@adaptivemobile.com>
2022-01-22 22:17:42 +0100

    hodina-1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kruzok-pc
 priklady a navody pre kruzok programovanie pre deti
+
+Uzitocne odkazy
+* https://geo-inf.sk/materialy-k-pythonu/
+
---
+++ hodina-1/priklad-1-1.py
@@ -0,0 +1,24 @@
+# poznamka (komentar) - riadok zacina so znakom '#'
+
+# vypisanie textu na obrazovku (na terminal, moze sa povedat: na konzolu)
+print('vypisanie mena a priezviska')
+
+# vypisanie text 'zadaj meno:'
+# a nacitanie textu do premennej 'meno'
+meno=input('zadaj meno:')
+# nacitanie priezviska do premennej 'priezvisko'
+priezvisko=input('zadaj priezvisko:')
+
+# vypisanie textu, medzery, obsahu premennej 'meno', medzery a obsahu 'priezvisko'
+print('meno a priezvisko:',meno,priezvisko)
+
+########################################################################
+# dalsie poznamky k prikladu
+#
+# viacriadkovy komentar - zaciatok a koniec je oznaceny znakmi (3 apostrofy): '''
+# napriklad:
+'''
+poznamka
+ma viac
+riadkov
+'''
