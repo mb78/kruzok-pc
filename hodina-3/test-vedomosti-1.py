53814146c9b96647e40e49ce4980201255e8f5b3
michal <michal.bella@adaptivemobile.com>
2022-01-31 00:22:30 +0100

    hodina-3

---
+++ hodina-3/test-vedomosti-1.py
@@ -0,0 +1,31 @@
+#!/bin/env python3
+
+# otvor ('open') subor 'matematika.test' a spristupni ho pomocou premennej 'subor'
+subor=open('matematika.test')
+
+# prvy riadok: zadanie alebo otazka s 3 moznostami
+otazka=subor.readline()
+
+# moznosti su oznacene a, b, c
+moznost_a=subor.readline()
+moznost_b=subor.readline()
+moznost_c=subor.readline()
+
+# subor po moznostiach obsahuje riadok so spravnou odpovedou, tiez a, b alebo c
+spravna_odpoved=subor.readline().strip()
+
+# vypisanie otazky zo suboru
+print(otazka)
+# a moznosti
+print('a)',moznost_a)
+print('b)',moznost_b)
+print('c)',moznost_c)
+
+# nacitaj odpoved
+odpoved=input('Zadaj odpoved (a, b alebo c):')
+
+# kontrola odpovede
+if (odpoved==spravna_odpoved):
+    print('Spravne !')
+else:
+    print('Nespravne ! odpoved',spravna_odpoved+')','bol spravna')
---
+++ hodina-3/test-vedomosti-3.py
@@ -0,0 +1,41 @@
+#!/bin/env python3
+
+# vylepsenie: ulohy sa zadaju na prikazovom riadku
+
+# nacitanie modulu (dalsich funkcii python-u) "sys"
+import sys
+
+# otvor ('open') subor 'matematika.test' a spristupni ho pomocou premennej 'subor'
+subor=open(sys.argv[1])
+
+def spracuj_otazku():
+    print('------------------------------------------')
+    # prvy riadok: zadanie alebo otazka s 3 moznostami
+    otazka=subor.readline()
+    # moznosti su oznacene a, b, c
+    moznost_a=subor.readline()
+    moznost_b=subor.readline()
+    moznost_c=subor.readline()
+
+    # subor po moznostiach obsahuje riadok so spravnou odpovedou, tiez a, b alebo c
+    spravna_odpoved=subor.readline().strip()
+
+    # vypisanie otazky zo suboru
+    print(otazka)
+    # a moznosti
+    print('a)',moznost_a)
+    print('b)',moznost_b)
+    print('c)',moznost_c)
+
+    # nacitaj odpoved
+    odpoved=input('Zadaj odpoved (a, b alebo c):')
+
+    # kontrola odpovede
+    if (odpoved==spravna_odpoved):
+        print('Spravne !')
+    else:
+        print('Nespravne ! odpoved',spravna_odpoved+')','bol spravna')
+
+spracuj_otazku()
+spracuj_otazku()
+spracuj_otazku()
