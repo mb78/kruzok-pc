3f862638443bd83dc8ed52eba37c505cb08f7380
mbella <michal.bella@example.test>
2025-02-14 17:50:55 +0100

    added tcp client connecting to a google server and downloading http response

---
+++ prog-c/01-tcp-client.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "142.251.37.110"  // Change this to the server IP if needed
+#define SERVER_PORT 80
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // Convert IP address from text to binary form
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //printf("Connected to server. Enter message: ");
+    //fgets(buffer, BUFFER_SIZE, stdin);
+    memcpy(buffer,"GET / HTTP/1.0\r\n\r\n",18);
+    send(sock, buffer, strlen(buffer), 0);
+
+    // Receive response
+    int bytes_received = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        printf("Server response: %s", buffer);
+    }
+
+    // Close socket
+    close(sock);
+    return 0;
+}
+
